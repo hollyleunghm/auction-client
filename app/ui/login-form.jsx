5c12ae3b09620a6c03789a9ee554f7f009a221f0
石博文 <7782436+Ritr@users.noreply.github.com>
2024-05-16 18:57:54 +0800

    添加手機號登錄

--- app/ui/login-form.jsx
+++ app/ui/login-form.jsx
@@ -2,18 +2,20 @@
 import { authenticate } from '@/lib/actions';
 import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
-import { MdOutlineMailOutline } from "react-icons/md";
+import { MdOutlineMailOutline, MdPhoneIphone } from "react-icons/md";
 import { RiLockPasswordLine } from "react-icons/ri";
 import Link from 'next/link';
 import { Button } from 'rsuite';
-
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import codeList from "@/app/register/code";
 export default function LoginForm() {
     const router = useRouter();
-
+    const [type, setType] = useState("email");
     const [loginStatus, setLoginStatus] = useState({ success: false, error: null, loading: false });
     const handleSubmit = async (event) => {
         event.preventDefault();
         const formData = new FormData(event.target);
+        formData.append("type", type);
         setLoginStatus({ success: false, error: null, loading: true })
         const result = await authenticate(null, formData);
         if (!result) {
@@ -30,76 +32,123 @@ export default function LoginForm() {
             location.href = localStorage.url;
         }
     }, [loginStatus, router]);
+    const LoginForm = ({ type = "email" }) => {
+        return (
+            <form onSubmit={handleSubmit} className="space-y-3">
+                <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
+                    <div className="w-full">
+                        {type === "email" ? (
+                            <div>
+                                <label
+                                    className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                    htmlFor="email"
+                                >
+                                    電郵
+                                </label>
+                                <div className="relative">
+                                    <input
+                                        className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                        id="email"
+                                        type="email"
+                                        name="email"
+                                        placeholder="請輸入電郵"
+                                        required
+                                    />
+                                    <MdOutlineMailOutline className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                                </div>
+                            </div>
+                        ) : (
+                            <div>
+                                <label
+                                    className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                    htmlFor="phone"
+                                >
+                                    電話
+                                </label>
+                                <div className="flex items-start gap-4">
+                                    <select name="code" id="code" className="indent-2 rounded-md border py-[9px] w-36  text-sm" required>
+                                        {
+                                            codeList.map((item, index) => {
+                                                return (
+                                                    <option key={index} value={item.code}>
+                                                        {item.cn}{item.code}
+                                                    </option>
+                                                )
+                                            })
+                                        }
+                                    </select>
+                                    <div className="relative">
+                                        <input
+                                            className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                            id="phone"
+                                            name="phone"
+                                            placeholder="請輸入電話"
+                                            required
+                                        />
+                                        <MdPhoneIphone className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                                    </div>
+                                </div>
 
+                            </div>
+                        )}
+                        <div className="mt-4">
+                            <label
+                                className="mb-3 mt-5 block text-xs font-medium text-gray-900"
+                                htmlFor="password"
+                            >
+                                密碼
+                            </label>
+                            <div className="relative">
+                                <input
+                                    className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
+                                    id="password"
+                                    type="password"
+                                    name="password"
+                                    placeholder="請輸入密碼"
+                                    required
+                                    minLength={6}
+                                />
+                                <RiLockPasswordLine className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="flex justify-between">
+                        <Link href="/register" className="text-sm text-gray-500 mt-2">前往注冊</Link>
+                        <Link href="/forget" className="text-sm text-gray-500 mt-2">忘記密碼</Link>
+                    </div>
+
+                    <Button appearance="primary" className="mt-4 w-full" type="submit" loading={loginStatus.loading}>
+                        開始拍賣
+                        {/* <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" /> */}
+                    </Button>
+                    <div
+                        className="flex h-8 items-end space-x-1"
+                        aria-live="polite"
+                        aria-atomic="true"
+                    >
+                        {loginStatus.error && (
+                            <>
+                                <p className="text-sm text-red-500">{loginStatus.error}</p>
+                            </>
+                        )}
+                    </div>
+                </div >
+            </form >
+        )
+    }
     return (
-        <form onSubmit={handleSubmit} className="space-y-3">
-            <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
-                <h1 className={`mb-3 text-2xl`}>
-                    登錄
-                </h1>
-                <div className="w-full">
-                    <div>
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="email"
-                        >
-                            電郵
-                        </label>
-                        <div className="relative">
-                            <input
-                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                id="email"
-                                type="email"
-                                name="email"
-                                placeholder="請輸入電郵/電話（不用區號）"
-                                required
-                            />
-                            <MdOutlineMailOutline className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-                        </div>
-                    </div>
-                    <div className="mt-4">
-                        <label
-                            className="mb-3 mt-5 block text-xs font-medium text-gray-900"
-                            htmlFor="password"
-                        >
-                            密碼
-                        </label>
-                        <div className="relative">
-                            <input
-                                className="peer block w-full rounded-md border border-gray-200 py-[9px] pl-10 text-sm outline-2 placeholder:text-gray-500"
-                                id="password"
-                                type="password"
-                                name="password"
-                                placeholder="請輸入密碼"
-                                required
-                                minLength={6}
-                            />
-                            <RiLockPasswordLine className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-                        </div>
-                    </div>
-                </div>
-                <div className="flex justify-between">
-                    <Link href="/register" className="text-sm text-gray-500 mt-2">前往注冊</Link>
-                    <Link href="/forget" className="text-sm text-gray-500 mt-2">忘記密碼</Link>
-                </div>
+        <Tabs defaultValue="email" className="w-full md:w-[400px]" onValueChange={value => { setType(value) }}>
+            <TabsList>
+                <TabsTrigger value="email">電郵登錄</TabsTrigger>
+                <TabsTrigger value="phone">電話登錄</TabsTrigger>
+            </TabsList>
+            <TabsContent value="email">
+                <LoginForm type="email"></LoginForm>
+            </TabsContent>
+            <TabsContent value="phone">
+                <LoginForm type="phone"></LoginForm>
+            </TabsContent>
+        </Tabs>
 
-                <Button appearance="primary" className="mt-4 w-full" type="submit" loading={loginStatus.loading}>
-                    開始拍賣
-                    {/* <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50" /> */}
-                </Button>
-                <div
-                    className="flex h-8 items-end space-x-1"
-                    aria-live="polite"
-                    aria-atomic="true"
-                >
-                    {loginStatus.error && (
-                        <>
-                            {/* <ExclamationCircleIcon className="h-5 w-5 text-red-500" /> */}
-                            <p className="text-sm text-red-500">{loginStatus.error}</p>
-                        </>
-                    )}
-                </div>
-            </div>
-        </form>
     );
 }
--- auth.js
+++ auth.js
@@ -2,8 +2,18 @@ import NextAuth from "next-auth";
 import Credentials from "next-auth/providers/credentials";
 import connectMongo from "@/lib/connect-mongo";
 import User from "@/models/user";
-import { AuthError } from "next-auth";
-export const { handlers: { GET, POST }, signIn, signOut, auth } = NextAuth({
+import {
+    AuthError
+} from "next-auth";
+export const {
+    handlers: {
+        GET,
+        POST
+    },
+    signIn,
+    signOut,
+    auth,
+} = NextAuth({
     providers: [
         Credentials({
             // You can specify which fields should be submitted, by adding keys to the `credentials` object.
@@ -13,32 +23,59 @@ export const { handlers: { GET, POST }, signIn, signOut, auth } = NextAuth({
                 password: {},
                 // name: String,
                 // abc: String,
-
             },
             authorize: async (credentials) => {
+                console.log(credentials);
                 let user = null;
                 await connectMongo();
-                user = await User.findOne({ email: credentials.email });
-                if (!user) {
-                    throw new AuthError("電郵尚未注冊");
+                if (credentials.type === "phone") {
+                    user = await User.findOne({
+                        phone: credentials.phone,
+                        code: credentials.code,
+                    });
+                    if (!user) {
+                        throw new AuthError("電話尚未注冊");
+                    }
+                    user = await User.findOne({
+                        phone: credentials.phone,
+                        code: credentials.code,
+                        password: credentials.password,
+                    });
+                    if (!user) {
+                        throw new AuthError("密碼錯誤");
+                    }
+                } else {
+                    user = await User.findOne({
+                        email: credentials.email
+                    });
+                    if (!user) {
+                        throw new AuthError("電郵尚未注冊");
+                    }
+                    user = await User.findOne({
+                        email: credentials.email,
+                        password: credentials.password,
+                    });
+                    if (!user) {
+                        throw new AuthError("密碼錯誤");
+                    }
                 }
-                user = await User.findOne({
-                    email: credentials.email,
-                    password: credentials.password,
-                });
-                if (!user) {
-                    throw new AuthError("電郵尚未注冊");
-                }
+
                 return user;
             },
         }),
     ],
     callbacks: {
-        async session({ session, token }) {
-            session.user = token.user
+        async session({
+            session,
+            token
+        }) {
+            session.user = token.user;
             return session;
         },
-        async jwt({ token, user }) {
+        async jwt({
+            token,
+            user
+        }) {
             if (user) {
                 token.user = user;
             }
