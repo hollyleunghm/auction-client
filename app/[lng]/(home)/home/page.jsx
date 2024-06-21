09ce241bf889487d419cba63d2a5d79f40c0e9f1
石博文 <7782436+Ritr@users.noreply.github.com>
2024-06-21 14:48:26 +0800

    配置tagline

--- app/[lng]/(home)/home/page.jsx
+++ app/[lng]/(home)/home/page.jsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import PropertyCarousel from "./propertyCarousel";
 import connectMongo from "@/lib/connect-mongo";
 import Property from "@/models/property";
+import Tagline from "@/models/tagline";
 import Change from "./change";
 import { useTranslation } from "@/app/i18n";
 import bg from "@/public/bg.webp";
@@ -12,14 +13,20 @@ export default async function Home({ params: { lng } }) {
     // const session = await auth();
     const { t } = await useTranslation(lng)
     await connectMongo();
-    const properties = await Property.find({ deleted: false });
+    const properties = await Property.find({ deleted: false, post: true });
+    const tagline = await Tagline.findOne({});
     return (
         <main className="">
             {/* Welcome to the home page!<p>Welcome {JSON.stringify(session)}</p> */}
             <div style={{ background: `url(${bg.src})` }} className={`bg-cover h-[50vh] md:h-[688px] md:py-36 flex justify-center items-center`}>
                 <div className="flex flex-col bg-[#253d59] py-8 md:py-12 px-12 md:px-36 w-full md:max-w-[900px] text-white text-center">
-                    <h1 className="md:text-4xl text-xl font-bold leading-normal" >PropBid普比德物業拍賣平台</h1>
-                    <h2 className="md:text-2xl text-md leading-normal mt-4">普比德為香港首創網上樓盤拍賣平台，提供住宅、車位、銀主盤、上車盤、二手樓盤等筍盤拍賣。物業遍佈香港島、九龍及新界。</h2>
+                    <h1 className="md:text-4xl text-xl font-bold leading-normal" >
+                        {lng === "zhcn" ? tagline.simplifiedChineseTitle : lng === "en" ? tagline.englishTitle : tagline.traditionalChineseTitle}
+                        {/* PropBid普比德物業拍賣平台 */}
+                    </h1>
+                    <h2 className="md:text-2xl text-md leading-normal mt-4">
+                        {lng === "zhcn" ? tagline.simplifiedChineseText : lng === "en" ? tagline.englishText : tagline.traditionalChineseText}
+                    </h2>
                     <Link href="/property">
                         <Button className="md:text-xl text-sm md:mt-16 mt-8 rounded-none bg-[#f0d300] text-black  px-20 py-4 transition-all hover:bg-[#f0d300] hover:opacity-80">{t("startAuction")}</Button>
                     </Link>
